4bf653f1f5322b257c8b836eca7017b887cb780e
kastriot <kastriotkosumi@hotmail.com>
2024-02-01 19:46:11 +0100

    removed CustomerDao not needed for now

--- src/main/java/com/example/demo/customer/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.customer;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface CustomerDao {
-    List<Customer> selectAllCustomers();
-    Optional<Customer> selectCustomerbyId();
-}
