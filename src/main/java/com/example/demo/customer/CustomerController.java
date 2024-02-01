ff07a7b8f0c22aff9f04f0359ed8e6929acba51f
kastriot <kastriotkosumi@hotmail.com>
2024-02-01 20:44:11 +0100

    Added some CRUD functions

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,10 +1,21 @@
 package com.example.demo.customer;
 
+import org.hibernate.internal.util.type.PrimitiveWrapperHelper.IntegerDescriptor;
+import org.simpleframework.xml.Path;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RequestMapping(path = "api/v1/customers")
 @RestController
@@ -20,5 +31,21 @@ public class CustomerController {
     public List<Customer> getAllCustomers(){
         return customerService.getAllCusotmers();
     }
+
+    @GetMapping("{CustomerID}")
+    public Optional<Customer> getCustomerById(@PathVariable("CustomerID") Integer id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @DeleteMapping("/{CusomerID}")
+    public void deleteCustomerById(@PathVariable("CusomerID") Integer id){
+        customerService.deleteCustomerById(id);
+    }
+    
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerService.CreateCustomer(customer);
+    }
+    
 
 }
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,14 +1,17 @@
 package com.example.demo.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
     private CustomerRepository customerRepository;
-    
+
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
@@ -16,4 +19,17 @@ public class CustomerService {
     public List<Customer> getAllCusotmers(){
         return customerRepository.findAll();
     }
+
+    public Optional<Customer> getCustomerById(Integer id){
+        return customerRepository.findById(id);
+    }
+
+    public void deleteCustomerById(Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    public Customer CreateCustomer(Customer customer){
+        return customerRepository.save(customer);
+    }
+
 }
