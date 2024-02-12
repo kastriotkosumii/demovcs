e86dd789c04bfd5e0b62400f995d033e15fc342d
Ismajlb <ismajl.bina@gmail.com>
2024-02-12 10:29:59 +0100

    Test added

--- src/test/java/com/example/demo/TestcontainersTest.java
+++ src/test/java/com/example/demo/TestcontainersTest.java
@@ -1,28 +1,14 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestcontainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("demovcs-dao-unit-test")
-                    .withUsername("demo")
-                    .withPassword("password");
+public class TestcontainersTest extends AbstractTestcontainers{
 
     @Test
     void canStartPostgresDB() {
-
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-
-
     }
 }
---
+++ src/test/java/com/example/demo/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.customer;
+
+import com.example.demo.AbstractTestcontainers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
+
+    private CustomerJDBCDataAccessService underTest;
+    private final CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CustomerJDBCDataAccessService(
+                getJdbcTemplate(),
+                customerRowMapper
+        );
+    }
+
+    @Test
+    void willReturnEmptyWhenSelectCustomerById() {
+        // Given
+        Long id = 0L;
+
+        // When
+        var actual = underTest.selectCustomerById(id);
+
+        // Then
+        assertThat(actual).isEmpty();
+    }
+
+
+}
